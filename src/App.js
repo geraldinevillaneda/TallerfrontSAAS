3e4d2d7b2bd1169e2ee7fb7e5fa67a4cae35ff50
Nilson28 <nilson.cardona@correounivalle.edu.co>
2021-01-20 22:38:28 -0500

    add new component for rendering points

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import logo from './logo.svg';
+import RenderPoints from './RenderPoint';
 import './App.css';
-import 'leaflet/dist/leaflet.css';
-import icono from 'leaflet/dist/images/marker-icon.png';
-import isombra from 'leaflet/dist/images/marker-shadow.png';
-import retina from 'leaflet/dist/images/marker-icon-2x.png';
-
-
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: retina,
-  iconUrl: icono,
-  shadowUrl: isombra,
-});
 
 const location = [4.08466, -76.19536];
 const locationPrueba = [4.08470, -76.19550];
@@ -23,17 +9,15 @@ const zoom = 12;
 const estacinesDeServicioTulua = {
 
 };
+
+
 function App() {
+ 
   return (
     <div className="App">
-      <MapContainer center={location} zoom={zoom}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
-        <Marker position={location}>
-          <Popup minWidth={90}>
-            Holii, Whashintong, DC
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div>
+      <RenderPoints />
+      </div>
     </div>
   );
 }
