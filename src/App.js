589415831d398d45e2570b082b7b6110f5f8cd09
Geraldine Lopez <glopez@fivetech.co>
2021-01-20 21:46:28 -0500

    Se completa el mensaje en la ubicacion

--- src/App.js
+++ src/App.js
@@ -20,9 +20,6 @@ const location = [4.08466, -76.19536];
 const locationPrueba = [4.08470, -76.19550];
 const zoom = 12;
 
-const estacinesDeServicioTulua = {
-
-};
 function App() {
   return (
     <div className="App">
@@ -30,7 +27,11 @@ function App() {
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
         <Marker position={location}>
           <Popup minWidth={90}>
-            Holii, Whashintong, DC
+            Estacion: <br />
+            Direccion: <br />
+            Telefono: <br />
+            Latitud: <br />
+            Longitud: <br />
           </Popup>
         </Marker>
       </MapContainer>
