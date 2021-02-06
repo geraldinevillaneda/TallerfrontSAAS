4ca7f8ff395fba4612657d401709b7667cd6e60d
Geraldine Lopez <glopez@fivetech.co>
2021-02-05 21:25:58 -0500

    error solucionado

--- src/App.js
+++ src/App.js
@@ -6,22 +6,18 @@ const location = [4.08466, -76.19536];
 const locationPrueba = [4.08470, -76.19550];
 const zoom = 12;
 
+const estacinesDeServicioTulua = {
+
+};
+
+
 function App() {
  
   return (
     <div className="App">
-      <MapContainer center={location} zoom={zoom}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
-        <Marker position={location}>
-          <Popup minWidth={90}>
-            Estacion: <br />
-            Direccion: <br />
-            Telefono: <br />
-            Latitud: <br />
-            Longitud: <br />
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div>
+      <RenderPoints />
+      </div>
     </div>
   );
 }
